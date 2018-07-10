6dfe88e501af7afaa3701059bc273bc1561eafc0
Emmanuel Mhlaba <emmanuel.mhlaba@fnb.co.za>
2018-07-10 11:31:25 +0200

    Created models for colours app.

--- colours_project/colours/models.py
+++ colours_project/colours/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Palette(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    slug = models.SlugField(unique=True) 
+
+    def __str__(self):
+        return self.name
+
+
+class Colour(models.Model):
+    palette = models.ForeignKey(Palette, on_delete=models.CASCADE)
+    name = models.CharField(max_length=128, unique=True)
+    hex_code = models.CharField(max_length=7, unique=True)
+    additional_info = models.TextField()
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
