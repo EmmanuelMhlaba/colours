742fc2d267270b90068e10eaeb1922172aa755d0
Emmanuel Mhlaba <emmanuel.mhlaba@fnb.co.za>
2018-07-13 10:58:24 +0200

    Added tests for palette list view.

--- colours_project/colours/tests/test_views.py
+++ colours_project/colours/tests/test_views.py
@@ -76,3 +76,50 @@ class PaletteDetailsViewTest(TestCase):
         res = self.client.get(self.p.get_absolute_url())
         self.assertEqual(res.status_code, 200)
         self.assertContains(res, "No colours available.")
+
+
+class PaletteListViewTest(TestCase):
+    def test_url_exists_at_desired_location(self):
+        res = self.client.get('/colours/palettes/')
+        self.assertEqual(res.status_code, 200)
+
+    def test_url_access_by_name(self):
+        res = self.client.get(reverse('colours:palette_list'))
+        self.assertEqual(res.status_code, 200)
+
+    def test_correct_template_used(self):
+        res = self.client.get(reverse('colours:palette_list'))
+        self.assertEqual(res.status_code, 200)
+        self.assertTemplateUsed(res, 'colours/palette_list.html')
+
+    def test_correct_heading_shown(self):
+        res = self.client.get(reverse('colours:palette_list'))
+        self.assertEqual(res.status_code, 200)
+        self.assertContains(res, "No palettes available.")
+
+    def test_palette_pagination_1(self):
+        for n in range(0, 7):
+            Palette.objects.create(name="Palette %s" % n)
+        res = self.client.get(reverse('colours:palette_list'))
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue('is_paginated' in res.context)
+        self.assertTrue(res.context['is_paginated'] == False)
+        self.assertTrue(len(res.context['palette_list']) == 7)
+
+    def test_palette_pagination_2(self):
+        for n in range(0, 8):
+            Palette.objects.create(name="Palette %s" % n)
+        res = self.client.get(reverse('colours:palette_list'))
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue('is_paginated' in res.context)
+        self.assertTrue(res.context['is_paginated'] == False)
+        self.assertTrue(len(res.context['palette_list']) == 8)
+
+    def test_palette_pagination_3(self):
+        for n in range(0, 10):
+            Palette.objects.create(name="Palette %s" % n)
+        res = self.client.get(reverse('colours:palette_list')+'?page=2')
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue('is_paginated' in res.context)
+        self.assertTrue(res.context['is_paginated'] == True)
+        self.assertTrue(len(res.context['palette_list']) == 2)
